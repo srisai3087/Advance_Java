f5622ef76895644470b24e0c82bb0100d4b8770c
SriSai <Suradasrisai5@gmail.com>
2026-02-12 12:14:30 +0530

    first commit

---
+++ src/main/java/com/sai/entity/Student.java
@@ -0,0 +1,42 @@
+package com.sai.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String city;
+
+    public Student() {}
+
+    public Student(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
